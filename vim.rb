9dc3bfab60ce359bed9f8823b2f7490597720565
Hanlin He <hanlhe@tetrationanalytics.com>
2019-02-13 14:12:29 -0800

    Update vim to 8.1.0900.

--- vim.rb
+++ vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.1.0800.tar.gz"
-  sha256 "ecc04dea54eb6b096c5df91a3ddbdfb19eebb2adf4aa481c758261555cbcf574"
+  url "https://github.com/vim/vim/archive/v8.1.0900.tar.gz"
+  sha256 "5a7cfa4dcaa96c41b32332ea5c6b9d1f6b3dc0a5f010ce7095ed15ae37eb0f33"
   head "https://github.com/vim/vim.git"
 
   option "with-override-system-vi", "Override system vi"
