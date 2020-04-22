f4a3b30cf21eb97fd38049bcee10df6a1450e5f0
Hanlin He <hanlhe@tetrationanalytics.com>
2020-04-22 13:33:40 -0700

    Add Iosevka font.

---
+++ Casks/font-iosevka.rb
@@ -0,0 +1,19 @@
+cask 'font-iosevka' do
+  version 'v3.0.0-rc.7'
+  sha256 '94c5e34f75f7099391168f7fe6cffa6ed9561a6ccc8f830b037e39de8ff2bf87'
+
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
+  appcast 'https://github.com/be5invis/Iosevka/releases.atom'
+  name 'Iosevka'
+  homepage 'https://github.com/be5invis/Iosevka/'
+
+  font 'iosevka-bold.ttc'
+  font 'iosevka-extrabold.ttc'
+  font 'iosevka-extralight.ttc'
+  font 'iosevka-heavy.ttc'
+  font 'iosevka-light.ttc'
+  font 'iosevka-medium.ttc'
+  font 'iosevka-regular.ttc'
+  font 'iosevka-semibold.ttc'
+  font 'iosevka-thin.ttc'
+end
