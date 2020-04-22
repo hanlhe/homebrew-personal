fe74356e8abdb8a16c48497e1d71d3cb7fc1cc3d
Hanlin He <hanlhe@tetrationanalytics.com>
2020-04-22 13:38:52 -0700

    Minor fix.

--- Casks/font-iosevka-beta.rb
+++ Casks/font-iosevka-beta.rb
@@ -1,5 +1,5 @@
 cask 'font-iosevka-beta' do
-  version 'v3.0.0-rc.7'
+  version '3.0.0-rc.7'
   sha256 '94c5e34f75f7099391168f7fe6cffa6ed9561a6ccc8f830b037e39de8ff2bf87'
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
